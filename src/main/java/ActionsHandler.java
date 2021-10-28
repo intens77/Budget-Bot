c2b3158defeb87b2efa366259ef75041c9cc1474
Vladislav Kirienko <v_kirienko7@mail.ru>
2021-10-29 00:21:46 +0500

    new commands for the bot and methods of the same name in the user class were added

--- src/main/java/ActionsHandler.java
+++ src/main/java/ActionsHandler.java
@@ -1,36 +1,84 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.HashMap;
-import java.util.Scanner;
 
 
 public class ActionsHandler {
 
-  private static HashMap<String, ICommand> commands;
+    private final HashMap<String, ICommand> commands;
+    private final HashMap<String, User> users;
 
-  public ActionsHandler() {
-    commands = new HashMap<>();
-    commands.put("/start", this::startProcess);
-    commands.put("/get_strategies", this::getStrategies);
-  }
-
-  public String processUserMessage(String message) {
-    if ((message != null) & (commands.containsKey(message))) {
-      return commands.get(message).execute();
+    public ActionsHandler() {
+        commands = new HashMap<>();
+        commands.put("/start", this::startProcess);
+        commands.put("/get_strategies", this::getStrategies);
+        commands.put("/increase_budget", this::increaseBudget);
+        commands.put("/decrease_budget", this::decreaseBudget);
+        commands.put("/set_budget", this::setBudget);
+        commands.put("/reset_budget", this::resetBudget);
+        users = new HashMap<>();
     }
-    return generateError();
-  }
 
-  public String startProcess() {
-    return SecondaryFunctions.readFileContent("start.txt");
-  }
+    public String processUserMessage(String userId, String message) {
+        if (!users.containsKey(userId))
+            users.put(userId, new User(userId));
+        if ((message != null) & (commands.containsKey(message))) {
+            return commands.get(message).execute(userId, message);
+        }
+        return generateError();
+    }
 
-  public String generateError() {
-    return "Я не знаю такую команду:(";
-  }
+    public String startProcess(String userId, String message) {
+        return SecondaryFunctions.readFileContent("start.txt");
+    }
 
-  public String getStrategies() {
-    return "Стратегия 1\n Стратегия 2\n Стратегия 3";
-  }
+    public String generateError() {
+        return "Я не знаю такую команду:(";
+    }
 
+    public String getStrategies(String userId, String message) {
+        return "Стратегия 1\n Стратегия 2\n Стратегия 3";
+    }
+
+    public String setBudget(String userId, String message) {
+        var currentUser = users.get(userId);
+        var operationResult = currentUser.setMonthBudget(Float.parseFloat(message));
+        if (operationResult)
+            return String.format("Отлично, Вы установили ваш " +
+                            "ежемесячный бюджет. Он составляет %s рублей",
+                    currentUser.checkMonthBudget());
+        return "Произошла ошибка. Скорее всего, " +
+                "Вы передали неверный параметр. Пожалуйста, прочитайте инструкцию и попробуйте снова";
+    }
+
+    public String resetBudget(String userId, String message) {
+        var currentUser = users.get(userId);
+        var operationResult = currentUser.resetMonthBudget(Float.parseFloat(message));
+        if (operationResult)
+            return String.format("Отлично, Вы переустановили ваш " +
+                            "ежемесячный бюджет. Он составляет %s рублей",
+                    currentUser.checkMonthBudget());
+        return "Произошла Ошибка. Скорее всего, " +
+                "Вы передали неверный параметр. Пожалуйста, прочитайте инструкцию и попробуйте снова";
+    }
+
+    public String increaseBudget(String userId, String message) {
+        var currentUser = users.get(userId);
+        var operationResult = currentUser.increaseMonthBudget(Float.parseFloat(message));
+        if (operationResult)
+            return String.format("Отлично, Вы увеличили ваш " +
+                            "ежемесячный бюджет. Он составляет %s рублей",
+                    currentUser.checkMonthBudget());
+        return "Произошла ошибка. Скорее всего, " +
+                "Вы передали неверный параметр. Пожалуйста, прочитайте инструкцию и попробуйте снова";
+    }
+
+    public String decreaseBudget(String userId, String message) {
+        var currentUser = users.get(userId);
+        var operationResult = currentUser.decreaseMonthBudget(Float.parseFloat(message));
+        if (operationResult)
+            return String.format("Отлично, Вы уменьшили ваш " +
+                            "ежемесячный бюджет. Он составляет %s рублей",
+                    currentUser.checkMonthBudget());
+        return "Произошла ошибка. Скорее всего, " +
+                "Вы передали неверный параметр. Пожалуйста, прочитайте инструкцию и попробуйте снова";
+    }
 }
--- src/test/java/Test_Bot.java
+++ src/test/java/Test_Bot.java
@@ -5,30 +5,30 @@ import org.junit.jupiter.api.Test;
 
 public class Test_Bot {
 
-  BudgetBot bot;
-  String message;
-  ActionsHandler actionsHandler;
+    BudgetBot bot;
+    String message;
+    ActionsHandler actionsHandler;
 
-  @BeforeEach
-  void setUp() {
-    bot = new BudgetBot();
-    actionsHandler = new ActionsHandler();
-    message = SecondaryFunctions.readFileContent("start.txt");
-  }
+    @BeforeEach
+    void setUp() {
+        bot = new BudgetBot();
+        actionsHandler = new ActionsHandler();
+        message = SecondaryFunctions.readFileContent("start.txt");
+    }
 
-  @Test
-  void checkStart() {
-    assertEquals(message, actionsHandler.processUserMessage("/start"));
-  }
+    @Test
+    void checkStart() {
+        assertEquals(message, actionsHandler.processUserMessage("/start"));
+    }
 
-  @Test
-  void testGetStrategy() {
-    assertEquals("Стратегия 1\n Стратегия 2\n Стратегия 3",
-        actionsHandler.processUserMessage("/get_strategies"));
-  }
+    @Test
+    void testGetStrategy() {
+        assertEquals("Стратегия 1\n Стратегия 2\n Стратегия 3",
+                actionsHandler.processUserMessage("/get_strategies"));
+    }
 
-  @Test
-  void testGenerateError() {
-    assertEquals("Я не знаю такую команду:(", actionsHandler.generateError());
-  }
+    @Test
+    void testGenerateError() {
+        assertEquals("Я не знаю такую команду:(", actionsHandler.generateError());
+    }
 }
